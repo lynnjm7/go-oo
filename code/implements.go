f4963e44bd09c35be293970a0e46696d6b55a32f
Josh Lynn <lynnjm7@gmail.com>
2016-05-04 01:02:30 -0700

    some reorganization

--- code/implements.go
+++ code/implements.go
@@ -1,5 +1,5 @@
 // myVec is essentially treated as a parameter to Magnitude
 // Now Vec2f implements the Magnituder interface
 func (myVec Vec2f) Magnitude() float {
-	return Math.Sqrt(myVec.x * myVec.x + myVec.y * myVec.y)
+	return math.Sqrt(myVec.x * myVec.x + myVec.y * myVec.y)
 }
